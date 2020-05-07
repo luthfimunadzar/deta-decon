f6b59254db69630433b1b1f872a28b631a49fea7
Luthfi Munadzar <lmunadzar@yahoo.com>
2020-05-07 22:38:24 +0700

    update socmed

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <b-container>
       <b-row>
-        <b-col md="4" lg="5">
+        <b-col md="4" lg="4">
           <h4>Contact</h4>
           <p>Telp: (021) 864 9905</p>
           <p>E-mail: detadecon94@yahoo.com </p>
@@ -13,7 +13,7 @@
           <p>Jl. Pondok Kelapa Raya, Duren Sawit,</p>
           <p>Jakarta – 13450, Indonesia.</p>
         </b-col>
-        <b-col md="3" lg="2" class="text-center text-md-left">
+        <b-col md="3" lg="3" class="text-center text-md-left">
           <h4 class="mt-3 mt-md-0">Follow</h4>
           <a href="https://www.facebook.com/DetaDecon/" target="_blank" class="socmed"><i class="fab fa-facebook-f"></i></a>
           <a href="https://www.linkedin.com/company/deta-decon" target="_blank" class="socmed"><i class="fab fa-linkedin-in"></i></a>
