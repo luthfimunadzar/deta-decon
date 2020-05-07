1a6158ed84398a1fb1379fcf116acb09e5c6178b
Luthfi Munadzar <lmunadzar@yahoo.com>
2020-05-07 17:23:30 +0700

    project add

--- pages/project.vue
+++ pages/project.vue
@@ -48,10 +48,10 @@
       <b-container>
         <b-row>
           <b-col>
-            <b-form inline class="form-search">
+            <b-form inline class="form-search" @submit.prevent="formSearch()">
               <label for="" class="mr-3">Filter Project</label>
               <b-form-input v-model="search" placeholder="Cari Berdasarkan Nama Project"></b-form-input>
-              <b-button variant="primary" @click.prevent="formSearch()">Cari</b-button>
+              <b-button variant="primary" type="submit">Cari</b-button>
             </b-form>
           </b-col>
         </b-row>
@@ -108,45 +108,46 @@ export default {
         },
       ],
       dataProject: [
+          // old
           {
               id: 1,
               nama: 'Gelora Bung Karno',
-              year: '2017 - 2018',
+              year: '2017-2018',
               cat: 'Supervision and Construction Management',
               img: '/project-1.jpg',
           },
           {
               id: 2,
               nama: 'Athlete Villages Kemayoran (Blok 10D2) Tower 3,4',
-              year: '2017 - 2018',
+              year: '2017-2018',
               cat: 'Supervision and Construction Management, Architecture Planning and Design',
               img: '/project-2.jpg',
           },
           {
               id: 3,
-              nama: 'Kampus UIN Suska Riau',
-              year: '2005 - 2008',
-              cat: 'Supervision and Construction Management',
+              nama: 'Management Construction & Supervision The Upgrading of Islamic University of Sultan Syarif Kasim (SUSKA) Project ',
+              year: '2006-2008',
+              cat: 'Construction Management',
               img: '/project-3.jpg',
           },
           {
               id: 4,
               nama: 'UIN Raden Patah Palembang',
-              year: '2017 - 2020',
+              year: '2017-2020',
               cat: 'Project Management & Supervision Consultant',
               img: '/project-4.jpg',
           },
           {
               id: 5,
               nama: 'UIN Sumatera Utara',
-              year: '2017 - 2020',
+              year: '2017-2020',
               cat: 'Project Management & Supervision Consultant',
               img: '/project-5.jpg',
           },
           {
               id: 6,
-              nama: 'UIN Sunan Ampel Surabaya',
-              year: '2012 - 2013',
+              nama: 'Project Management and Supervision Consultant of  UIN Sunan Ampel',
+              year: '2013-2015',
               cat: 'Project Management & Supervision Consultant',
               img: '/project-6.jpg',
           },
@@ -159,9 +160,9 @@ export default {
           },
           {
               id: 8,
-              nama: 'Bank Indonesia Building',
-              year: '2009',
-              cat: 'Structural Design',
+              nama: 'Multifunction Koperbi Building Bank of Indonesia',
+              year: '2008',
+              cat: 'Detail Engineering Design (Public Buildings & Infra Structure)',
               img: '/project-8.jpg',
           },
           {
@@ -171,6 +172,151 @@ export default {
               cat: 'Project Management & Supervision Consultant',
               img: '/project-9.jpg',
           },
+          // ded
+          {
+              id: 10,
+              nama: 'The Upgrading and Development of The Semarang State University',
+              year: '2013',
+              cat: 'Detail Engineering Design (Educational Building)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 11,
+              nama: 'The Upgrading Development Of State Islamic University Of Sunan Gunung Djati Bandung',
+              year: '2010',
+              cat: 'Detail Engineering Design (Educational Building)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 12,
+              nama: 'University of Alauddin Makassar',
+              year: '2007-2008',
+              cat: 'Detail Engineering Design (Educational Building)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 13,
+              nama: 'The Development and Upgrading of STAIN Malang Project',
+              year: '2005',
+              cat: 'Detail Engineering Design (Educational Building)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 14,
+              nama: 'Detail Engineering Desain for Auditorium/ Multipurpose Building of The State University of Gorontalo.',
+              year: '2011',
+              cat: 'Detail Engineering Design (Educational Building)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 15,
+              nama: 'Islamic Centre Jakarta',
+              year: '2005, 2006, 2007',
+              cat: 'Detail Engineering Design (Public Buildings & Infra Structure)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 16,
+              nama: 'Master Plan & DED Of Menteng Park, Cental of Jakarta',
+              year: '2006',
+              cat: 'Detail Engineering Design (Public Buildings & Infra Structure)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 17,
+              nama: 'Detail Engineering Desain of Training Centre of Pusdiklat Pajak, Ministry of Finance (2014), 11 th Floors + 1 Basement',
+              year: '2014',
+              cat: 'Detail Engineering Design (Public Buildings & Infra Structure)',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          // mk
+          {
+              id: 18,
+              nama: 'Construction Management “Hotel Grandika Project – Blok M”',
+              year: '2013-2014',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 19,
+              nama: 'Construction Management of Continous Class III Hospital Building, RSUD Arifin Achmad',
+              year: '2013',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 20,
+              nama: 'Construction Management of Riau Province Development Office',
+              year: '2012-2013',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 21,
+              nama: 'Work on Construction Management of the Phase II Annex Building Renovation Project and Mosque PT. ASKES',
+              year: '2010',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 22,
+              nama: 'Construction Management of the Phase I Annex Building Renovation Project and Mosque PT. ASKES',
+              year: '2009',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 23,
+              nama: 'Construction Management Building Construction Work B Regional Office Directorate General of Customs and Excise, Jakarta',
+              year: '2010',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 24,
+              nama: 'PPM Building Construction Management Construction Work',
+              year: '2009-2010',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 25,
+              nama: 'Construction Management Construction Madrasa International Standard',
+              year: '2009',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 26,
+              nama: 'Construction Management Construction of East Jakarta Mayor Office Building Block C and D - 16 floors (multi years)',
+              year: '2007-2009',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          {
+              id: 27,
+              nama: 'Construction Management of Riau Main Stadium Construction',
+              year: '2008',
+              cat: 'Construction Management',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          // PMSC
+          {
+              id: 28,
+              nama: 'Project Management And Supervision Consultant, The Development and Upgrading of The State University of Jakarta (UNJ)',
+              year: '2011-2013',
+              cat: 'Project Management & Supervision Consultant',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          // Supervision
+          {
+              id: 28,
+              nama: 'Rusunawa Supervision Development Work - 2 (PP10-17)',
+              year: '2010-2011',
+              cat: 'Supervision Consultant',
+              img: 'https://place-hold.it/426x261?text=Project+Image',
+          },
+          // Building Audit
       ],
       search: "",
     }
@@ -181,6 +327,7 @@ export default {
   methods: {
     filtered(params){
       let tempFiltered = this.dataProject.filter( e => e.cat.toLowerCase().includes(params.toLowerCase()));
+      this.keyword = params
 
       this.dataShow = tempFiltered
     },
